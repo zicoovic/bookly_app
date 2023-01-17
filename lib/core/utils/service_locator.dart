9c7ecb98c12b561d983f9b95a4d5d646a6b635b8
zicoovic <abdelrhmanzakariaqa@gmail.com>
2023-01-17 12:50:25 +0300

    setup service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -6,7 +6,7 @@ import 'api_service.dart';
 
 final getIt = GetIt.instance;
 
-void setup() {
+void setupServiceLocator() {
   getIt.registerSingleton<ApiService>(
     ApiService(
       Dio(),
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
