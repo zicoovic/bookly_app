e238892fcaad34e9dc443377aed1a1153d126453
zicoovic <abdelrhmanzakariaqa@gmail.com>
2023-01-17 08:26:03 +0300

    finish featured books cubit

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBoxCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBoxCubit() : super(FeaturedBooksInitial());
+  FeaturedBoxCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failure) => emit(FeaturedBooksFailure(failure.errMessage)),
+      (books) => emit(FeaturedBooksSuccess(books)),
+    );
+  }
 }
